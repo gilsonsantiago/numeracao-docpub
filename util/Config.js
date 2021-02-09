a3ac91aa2fc9878b6a500e56f8ef19429a374ca6
gsthiago <gs.thiago@hotmail.com>
2021-02-09 01:00:01 -0300

    Revert "Commit Inicial."
    This reverts commit 3518625b8faee7af0711471321b88295432522cd.

--- util/Config.js
+++
@@ -1,9 +0,0 @@
-const empresa = {
-
-     nome : 'PREFEITURA MUNICIPAL DE MONTADAS',
-     endereco : 'Rua José Veríssimo de Souza, 106 - Centro, Montadas - PB',
-     contato : 'prefeituramontadas2013@gmail.com'
-
-}
-
-module.exports = empresa;
