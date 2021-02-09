7cd310f092be28b0ac750d7746529e666f8cbd65
gsthiago <gs.thiago@hotmail.com>
2021-02-09 01:05:10 -0300

    Commit Inicial.

---
+++ util/Config.js
@@ -0,0 +1,9 @@
+const empresa = {
+
+     nome : 'NOME DA EMPRESA',
+     endereco : 'Rua José Veríssimo de Souza, 106 - Centro, Montadas - PB',
+     contato : 'email@email.com'
+
+}
+
+module.exports = empresa;
